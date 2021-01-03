b1903fa3119897b2586a0de4274c8cdeeb02b8cd
junhyek son <nomoory@gmail.com>
2021-01-03 13:01:05 +0900

    Change image route

--- pages/edmicbio/team/components/Team.js
+++ pages/edmicbio/team/components/Team.js
@@ -48,39 +48,39 @@ const MEMBERS = [
     name: "하동헌",
     position: "CEO",
     positionSub: "(Chief Executive Officer)",
-    photo: "/images/edmicbio/team/hdh.png",
+    photo: "https://firebasestorage.googleapis.com/v0/b/edmicbio.appspot.com/o/edmicbio%2Fteam%2Fhdh.png?alt=media&token=1e06e61f-68e6-414f-b2c9-8025f4a0a08c",
   },
   {
     name: "이형석",
     position: "CTO",
     positionSub: "(Chief Technology Officer)",
-    photo: "/images/edmicbio/team/lhs.png",
+    photo: "https://firebasestorage.googleapis.com/v0/b/edmicbio.appspot.com/o/edmicbio%2Fteam%2Flhs.png?alt=media&token=ba7e34bf-f83e-444d-9c35-1dfc3760177b",
   },
   {
     name: "조동우",
     position: "기술고문",
-    photo: "/images/edmicbio/team/jdw.JPG",
+    photo: "https://firebasestorage.googleapis.com/v0/b/edmicbio.appspot.com/o/edmicbio%2Fteam%2Fjdw.JPG?alt=media&token=6ffd0b62-0dcd-43d3-bab4-1c16e2aefec2",
   },
   {
     name: "장진아",
     position: "기술고문",
-    photo: "/images/edmicbio/team/jja.png",
+    photo: "https://firebasestorage.googleapis.com/v0/b/edmicbio.appspot.com/o/edmicbio%2Fteam%2Fjja.png?alt=media&token=cf7732a6-2f6f-413b-a1d1-b6b4712c76c1",
   },
   {
     name: "이한철",
     position: "기술고문",
-    photo: "/images/edmicbio/team/lhc.png",
+    photo: "https://firebasestorage.googleapis.com/v0/b/edmicbio.appspot.com/o/edmicbio%2Fteam%2Flhc.png?alt=media&token=c6f00854-4500-45b3-a693-060a3b798373",
   },
   {
     name: "박태준",
     position: "CFO",
     positionSub: "(Chief Financial Officer)",
-    photo: "/images/edmicbio/team/btj.JPG",
+    photo: "https://firebasestorage.googleapis.com/v0/b/edmicbio.appspot.com/o/edmicbio%2Fteam%2Fbtj.jpg?alt=media&token=28a825b5-75a4-4eb4-933c-1c6f63e547df",
   },
   {
     name: "이승록",
     position: "연구원",
-    photo: "/images/edmicbio/team/lsl.png",
+    photo: "https://firebasestorage.googleapis.com/v0/b/edmicbio.appspot.com/o/edmicbio%2Fteam%2Flsl.png?alt=media&token=20df9947-dd49-4836-99d5-9e933909dda5",
   },
 ];
 
